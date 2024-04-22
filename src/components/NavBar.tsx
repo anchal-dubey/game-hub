134edf6580c94bc7265611e26dfac835b025f97c
anchald <anchald@cdac.in>
2024-04-22 09:01:02 +0000

    added dark mode toggling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import { HStack, Image, Text } from "@chakra-ui/react"
 import logo from "../assets/logo.webp" ;
+import ColourModeSwitch from "./ColourModeSwith";
 function NavBar(){
   return (
-    <HStack>
+    <HStack justifyContent='space-between' padding="10px">
       <Image src={logo} boxSize="60px"/> {/*we have to pass object insted of path of the asset */}
-      <Text >Nav Bar</Text>
+      <ColourModeSwitch />
     </HStack>
   )
 };
